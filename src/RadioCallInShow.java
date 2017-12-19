2b0aa92b857db326bde12171b4e75f5d09ecbb97
kaib8230 <kaib8230@ISS2TNQKH2.scdsb.pri>
2017-12-19 10:11:53 -0500

    Finished lesson and added PriorityQueue

--- src/RadioCallInShow.java
+++ src/RadioCallInShow.java
@@ -8,9 +8,21 @@ public class RadioCallInShow {
         System.out.println("Currently we have "+phoneline.size() +" callers waiting");
         System.out.println("3 calls are coming in");
         phoneline.enqueue(new Caller("Jim"));
-        phoneline.enqueue(new Caller("Amy"));
-        phoneline.enqueue(new Caller("Susan"));
+        phoneline.enqueue(new Caller("Pam"));
+        phoneline.enqueue(new Caller("Dwight"));
         System.out.println("The next caller will be: "+phoneline.peekFront());
+        System.out.println("We are now talking with "+phoneline.dequeue());
+        System.out.println("Currently we have "+phoneline.size() +" callers waiting");
+        System.out.println("Two more people have called in and are on hold..");
+        phoneline.enqueue(new Caller("Michael"));
+        phoneline.enqueue(new Caller("Kevin"));
+        System.out.println("Let's take the next call - this is :"+phoneline.dequeue());
+        System.out.println("We've run out of time, let's clear the lines and say a quick hello to everyone");
+        while(phoneline.size()>0){
+            System.out.println("Hello "+phoneline.dequeue()+", thanks for calling");
+        }
+        System.out.println("Currently we have "+phoneline.size()+" callers waiting..");
     }
+    
     
 }
